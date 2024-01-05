a7ef1effc6bce7363c2077f792911840ec35a55a
Steven Sean <133088404+Steven-Sean@users.noreply.github.com>
2024-01-05 18:15:08 +0700

    Add default role for registration

--- laravel/app/Models/User.php
+++ laravel/app/Models/User.php
@@ -24,6 +24,10 @@ class User extends Authenticatable
         'password',
     ];
 
+    protected $attributes = [
+        'role' => 'user'
+    ];
+
     /**
      * The attributes that should be hidden for serialization.
      *
