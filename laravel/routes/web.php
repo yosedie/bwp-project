4fb5489aa2111abf462c6e1ec8b1be85d29b08d0
baritones <obertjulian@gmail.com>
2024-01-06 22:09:53 +0700

    another routing changes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
+
+Route::get('/', function () {
+    return redirect()->route('login');
+});
 Route::get('/AddAdmin', function () {
     return view('admin.AddAdmin');
 });
