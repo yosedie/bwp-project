1818aaef2790e51c85f01df8fa41dc1111024cfd
Steven Sean <133088404+Steven-Sean@users.noreply.github.com>
2024-01-07 13:45:04 +0700

    Add search functionality to home
    no searchbox yet

--- laravel/app/Http/Controllers/ContentController.php
+++ laravel/app/Http/Controllers/ContentController.php
@@ -8,9 +8,15 @@ use Illuminate\Http\Request;
 
 class ContentController extends Controller
 {
-    public function getAllContent()
+    public function getAllContent(Request $request)
     {
-        $content = Content::all();
+        $query = $request->string('query')->trim();
+        if ($query) {
+            $content = Content::where('title', 'LIKE', '%'.$query.'%')->get();
+        } else {
+            $content = Content::all();
+        }
+        
         return view('home', compact('content'));
     }
 }
