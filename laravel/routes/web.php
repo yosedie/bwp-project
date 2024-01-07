d096baed758e01866aa7236cf9b5bcc33b0e1961
v <ryanyosedieirawan@gmail.com>
2024-01-07 12:18:23 +0700

    bug fix
    buat route

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -14,25 +14,19 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
     return redirect()->route('login');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
-
-Route::get('/admin', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified', 'admin'])->name('admin');
-
 Route::get('/home', [ContentController::class, 'getAllContent'])->middleware(['auth', 'verified'])->name('users');
-
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('dashboard');
+Route::get('/admin', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified', 'admin'])->name('admin');
 require __DIR__ . '/auth.php';
