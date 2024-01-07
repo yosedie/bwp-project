bf9e553c557933a1e1c8cc862b4f7d6d9833dc5e
v <ryanyosedieirawan@gmail.com>
2024-01-07 12:03:18 +0700

    ke register
    rute

--- laravel/resources/views/auth/login.blade.php
+++ laravel/resources/views/auth/login.blade.php
@@ -31,14 +31,16 @@
                 <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
             </label>
         </div>
-
+         
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
+        @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
             @endif
 
+        <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
+            
             <x-primary-button class="ms-3">
                 {{ __('Log in') }}
             </x-primary-button>
