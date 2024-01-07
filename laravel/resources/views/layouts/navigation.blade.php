7622ce8815dc490fc1ecb506fe3ce9d7b1965c12
yosua_christian <qian.liem.christian@gmail.com>
2024-01-07 14:14:30 +0700

    mencobaaddadmin

--- laravel/resources/views/layouts/navigation.blade.php
+++ laravel/resources/views/layouts/navigation.blade.php
@@ -17,13 +17,8 @@
                     </x-nav-link>
                 </div>
                 <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Admin') }}
-                    </x-nav-link>
-                </div>
-                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('AddAdmin') }}
+                    <x-nav-link :href="route('alladmin')" :active="request()->routeIs('alladmin')">
+                        {{ __('AllAdmin') }}
                     </x-nav-link>
                 </div>
             </div>
