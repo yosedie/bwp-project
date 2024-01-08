268f081fe7b8350432b3d4d41a1aba1bfb25084e
v <ryanyosedieirawan@gmail.com>
2024-01-08 10:56:17 +0700

    penggunaan adminLTE secara simple

--- laravel/resources/views/channel.blade.php
+++ laravel/resources/views/channel.blade.php
@@ -10,6 +10,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+  {{-- <script src="https://cdn.tailwindcss.com?plugins=forms,typography,aspect-ratio,line-clamp"></script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
 </head>
 
@@ -95,7 +98,7 @@
                             @csrf
                             <button type="submit" class="btn btn-primary">Subscribe</button>
                         </form>
-
+                        <br>
                         <!-- Tombol untuk follow -->
                         <form action="{{ route('follow.channel', ['id' => $item->id]) }}" method="post">
                             @csrf
