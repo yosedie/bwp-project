59fb8dd90c11a31c8d4ef5b5ecc326120fa3672a
v <ryanyosedieirawan@gmail.com>
2024-01-08 11:03:48 +0700

    adminlte again

--- laravel/resources/views/detail.blade.php
+++ laravel/resources/views/detail.blade.php
@@ -10,6 +10,9 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
     </script>
+    
+    <script src="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/js/adminlte.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/admin-lte@3.2/dist/css/adminlte.min.css">
 
 </head>
 
